39b268c217f3059b6d5222bad1884dbccdb9033a
Harsh Kushwaha <harsh2004great@gmail.com>
2025-06-26 09:14:26 +0530

    Add files via upload

---
+++ db/Sales.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const saleSchema = new mongoose.Schema({
+  itemID: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'HandcraftedItem', 
+    required: true 
+  },
+  artisanID: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Artisan', 
+    required: true 
+  },
+  platformName: { 
+    type: String, 
+    required: true 
+  },
+  quantitySold: { 
+    type: Number, 
+    required: true 
+  },
+  totalRevenue: { 
+    type: Number, 
+    required: true 
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+module.exports = mongoose.model('Sale', saleSchema);
