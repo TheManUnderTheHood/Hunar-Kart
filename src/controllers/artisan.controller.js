b999621043dcab5c4a368abb98f271e03f4d2751
Harsh Kushwaha <harsh2004great@gmail.com>
2025-07-07 14:53:19 +0000

    added casacading deleting of artisan data

--- src/controllers/artisan.controller.js
+++ src/controllers/artisan.controller.js
@@ -2,6 +2,11 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Artisan } from "../models/Artisan.model.js";
+import { HandcraftedItem } from "../models/HandcraftedItem.model.js";
+import { AgreementDocument } from "../models/AgreementDocument.model.js";
+import { Sale } from "../models/Sales.model.js";
+import { PlatformListing } from "../models/PlatformListing.model.js";
+import { removeFromCloudinary } from "../utils/cloudinary.js";
 import mongoose from "mongoose";
 
 const createArtisan = asyncHandler(async (req, res) => {
@@ -89,15 +94,58 @@ const deleteArtisan = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Invalid Artisan ID");
     }
 
-    const artisan = await Artisan.findByIdAndDelete(artisanId);
+    const session = await mongoose.startSession();
 
-    if (!artisan) {
-        throw new ApiError(404, "Artisan not found");
+    try {
+        session.startTransaction();
+
+        const artisan = await Artisan.findById(artisanId).session(session);
+        if (!artisan) {
+            throw new ApiError(404, "Artisan not found");
+        }
+
+        const agreements = await AgreementDocument.find({ artisanID: artisanId }).session(session);
+        if (agreements.length > 0) {
+
+            const publicIds = agreements.map(doc => doc.filePathPublicId).filter(id => id);
+            if (publicIds.length > 0) {
+                publicIds.forEach(id => removeFromCloudinary(id)); 
+            }
+
+            await AgreementDocument.deleteMany({ artisanID: artisanId }).session(session);
+        }
+
+        const items = await HandcraftedItem.find({ artisanID: artisanId }).select('_id').session(session);
+        const itemIds = items.map(item => item._id);
+
+        if (itemIds.length > 0) {
+
+            await PlatformListing.deleteMany({ itemID: { $in: itemIds } }).session(session);
+
+            await HandcraftedItem.deleteMany({ artisanID: artisanId }).session(session);
+        }
+
+        await Sale.deleteMany({ artisanID: artisanId }).session(session);
+
+        await Artisan.findByIdAndDelete(artisanId).session(session);
+
+        await session.commitTransaction();
+        
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Artisan and all related data deleted successfully")
+        );
+
+    } catch (error) {
+        await session.abortTransaction();
+
+        throw new ApiError(
+            error.statusCode || 500, 
+            error.message || "An error occurred while deleting the artisan and related data."
+        );
+
+    } finally {
+        session.endSession();
     }
-
-    return res.status(200).json(
-        new ApiResponse(200, {}, "Artisan deleted successfully")
-    );
 });
 
 export { createArtisan, getAllArtisans, getArtisanById, updateArtisan, deleteArtisan };
