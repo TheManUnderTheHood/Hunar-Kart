2fbb471ecc89d614d257f0fe57b05f0a5772a6f5
Harsh-6398 <chaharharsh67@gmail.com>
2025-11-11 10:59:26 +0530

    fix

--- Backend/src/utils/ApiError.js
+++ Backend/src/utils/ApiError.js
@@ -1,6 +1,7 @@
 class ApiError extends Error{
     constructor(statusCode, message = "Something went wrong", errors= [], statck = ""){
         super(message)
+        
         this.statusCode = statusCode
         this.data = null
         this.message = message
